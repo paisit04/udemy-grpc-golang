f0642af4320b3a85cda73318d34160ba654fee68
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-04-20 02:06:51 -0700

    Section 7: [Hands-On] gRPC Client Streaming

---
+++ calculator/server/avg.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	pb "github.com/paisit04/udemy-grpc-golang/calculator/proto"
+)
+
+func (s *Server) Avg(stream pb.CalculatorService_AvgServer) error {
+	log.Println("Avg function was invoked")
+
+	var sum int32 = 0
+	count := 0
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.AvgResponse{
+				Result: float64(sum) / float64(count),
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading client stream %v\n", err)
+		}
+
+		log.Printf("Receiving: %d\n", req.Number)
+
+		sum += req.Number
+		count++
+	}
+}
