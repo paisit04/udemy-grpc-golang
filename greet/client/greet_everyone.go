edd254494c815d0604b8b76825fdd4b2d7c25801
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-04-20 04:36:21 -0700

    Section 8: [Hands-On] gRPC Bi-Directional Streaming

---
+++ greet/client/greet_everyone.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/paisit04/udemy-grpc-golang/greet/proto"
+)
+
+func doGreetEveryone(c pb.GreetServiceClient) {
+	log.Println("doGreetEveryone was invoked")
+
+	stream, err := c.GreetEveryone(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error while creating stream: %v\n", err)
+	}
+
+	reqs := []*pb.GreetRequest{
+		{FirstName: "Clement"},
+		{FirstName: "Marie"},
+		{FirstName: "Test"},
+	}
+
+	waitc := make(chan struct{})
+
+	go func() {
+		for _, req := range reqs {
+			log.Printf("Send request: %v\n", req)
+			stream.Send(req)
+			time.Sleep(1 * time.Second)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				log.Printf("Error while receiving: %v\n", err)
+				break
+			}
+
+			log.Printf("Recieved: %v\n", res.Result)
+		}
+
+		close(waitc)
+	}()
+
+	<-waitc
+}
