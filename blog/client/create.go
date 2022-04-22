d1735fef8aa8bd8b66f7d300dd2e79368ed90df5
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-04-22 08:22:43 -0700

    Section 10: [Hands-On] CRUD API with MongoDB

---
+++ blog/client/create.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/paisit04/udemy-grpc-golang/blog/proto"
+)
+
+func createBlog(c pb.BlogServiceClient) string {
+	log.Println("---createBlog was invoked---")
+
+	blog := &pb.Blog{
+		AuthorId: "Clement",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+
+	res, err := c.CreateBlog(context.Background(), blog)
+
+	if err != nil {
+		log.Fatalf("Unexpected error: %v\n", err)
+	}
+
+	log.Printf("Blog has been created: %s\n", res.Id)
+
+	return res.Id
+}
